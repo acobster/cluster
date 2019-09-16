fabf7581bbb3b52fc9bf614e0d363428ffe4a6b3
Coby Tamayo <acobster@gmail.com>
2019-09-15 19:16:23 -0700

    get firewall rules working

--- cluster.tf
+++ cluster.tf
@@ -114,6 +114,35 @@ resource "digitalocean_firewall" "flynn_firewall" {
       port_range         = "3000-3500"
       source_addresses   = ["0.0.0.0/0", "::/0"]
   }
+
+  outbound_rule {
+      protocol                = "tcp"
+      port_range              = "80"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+      protocol                = "tcp"
+      port_range              = "443"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+      protocol                = "udp"
+      port_range              = "53"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+      protocol                = "tcp"
+      port_range              = "53"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+      protocol                = "icmp"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+  }
 }
 
 
